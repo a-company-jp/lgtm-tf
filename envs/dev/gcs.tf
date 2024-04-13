0d83ecf11e7c68cebed65a2fad691b8bdd6d05c2
Shion Ichikawa <shion1305@gmail.com>
2024-04-13 15:11:01 +0900

    🛂 GCSの権限設定更新

--- envs/dev/gcs.tf
+++ envs/dev/gcs.tf
@@ -1,9 +1,3 @@
-resource "google_storage_bucket_access_control" "public_image_bucket" {
-  bucket = google_storage_bucket.public_image_bucket.name
-  role   = "READER"
-  entity = "allUsers"
-}
-
 resource "google_storage_bucket" "public_image_bucket" {
   name     = "lgtm-public-image-bucket"
   location = "ASIA-NORTHEAST1"
@@ -18,4 +12,19 @@ resource "google_storage_bucket" "public_image_bucket" {
     response_header = ["*"]
     max_age_seconds = 3600
   }
+}
+
+resource "google_storage_bucket_access_control" "public_image_bucket" {
+  bucket = google_storage_bucket.public_image_bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_bucket_iam_binding" "public_image_bucket_editor" {
+  bucket = google_storage_bucket.public_image_bucket.name
+  role   = "roles/storage.objectAdmin"
+
+  members = [
+    "group:dev-members@a.shion.pro"
+  ]
 }
