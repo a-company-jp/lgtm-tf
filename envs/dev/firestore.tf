dd102b2161a8024f56895fb3407915c7d7bb0b0c
Shion Ichikawa <shion1305@gmail.com>
2024-04-13 13:46:12 +0900

    🔧 enable firestore

--- envs/dev/firestore.tf
+++ envs/dev/firestore.tf
@@ -1,3 +1,7 @@
+resource "google_project_service" "firestore_api" {
+  service = "firestore.googleapis.com"
+}
+
 resource "google_firestore_database" "datastore_mode_database" {
   project     = "my-project-name"
   name        = "(default)"
