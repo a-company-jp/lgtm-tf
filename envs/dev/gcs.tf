c122d17ff6626e490cad8799cd1d13378aa1c687
Shion Ichikawa <shion1305@gmail.com>
2024-04-13 12:07:16 +0900

    🐛 fix gcs location

--- envs/dev/gcs.tf
+++ envs/dev/gcs.tf
@@ -5,8 +5,9 @@ resource "google_storage_bucket_access_control" "public_image_bucket" {
 }
 
 resource "google_storage_bucket" "public_image_bucket" {
-  name          = "lgtm-public-image-bucket"
-  location      = "ap-northeast-1"
+  name     = "lgtm-public-image-bucket"
+  location = "ASIA-NORTHEAST1"
+  # https://cloud.google.com/storage/docs/locations
   force_destroy = false
 
   uniform_bucket_level_access = true
